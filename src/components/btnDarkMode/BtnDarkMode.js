74dd85ebd8ab89d623ed248f863492a891ed74ee
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-02-17 22:29:12 +0200

    adding localStorage

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useLocalStorage } from 'utils/useLocalStorage';
 import sun from './../../img/icons/sun.svg';
 import moon from './../../img/icons/moon.svg';
 import './styles.css';
 
 const BtnDarkMode = () => {
-  const [darkMode, setDarkMode] = useState('light');
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', 'light');
   const btnRef = useRef(null);
 
   useEffect(() => {
