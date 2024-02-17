c1b09ad8ec78d393fc7183fd31bace9a9b7a4003
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-02-17 12:31:21 +0200

    adding pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './styles/main.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
-import Home from 'pages/Home';
-import Projects from 'pages/Projects';
+// import Home from 'pages/Home';
+// import Projects from 'pages/Projects';
 import Contacts from 'pages/Contacts';
 
 function App() {
